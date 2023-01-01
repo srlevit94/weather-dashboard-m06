740cb01c7caf5689ed5a7ede4a2e4a566e7a1297
shanelevites <srlevit94@gmail.com>
2022-12-31 21:09:36 -0800

    current and 5day forecast load properly; need to make page responsive and make buttons for searched cities

--- Develop/script.js
+++ Develop/script.js
@@ -28,22 +28,21 @@ const getCurrentWeather = () => {
             var currentDate = dayjs().format('MM/DD/YYYY');
             //fills in HTML based on API response
             let currentHTML = `
-            <div id="current-weather" class="col">
-                    <h2> Today's Forecast:</h2>
-                    <h3><span id="city-name">${response.name} </span><span id="current-day">${currentDate}</span><img src="http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png"></h3>
+            <div id="" class="col p-3">
+                    <h2>Current Weather in ${response.name} ${currentDate} <img src="http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png"></h2>
                     <p><span id="current-temp">Temperature: ${response.main.temp} °F</span></p>
                     <p><span id="current-hum">Humidity: ${response.main.humidity}%</span></p>
-                    <p><span id="current-wind">Wind Speed: ${response.wind.speed} mph</span></p>
+                    <p><span id="current-wind">Wind: ${response.wind.speed} mph</span></p>
             </div>
             `;
-        getFiveDayForecast(event);
+        getFiveDayForecast();
         // adds HTML based on id
         $('#current-weather').html(currentHTML);
         })
 }
 
 // function to get five-day forecast
-const getFiveDayForecast = (event) => {
+const getFiveDayForecast = () => {
     let searchedCity = $('#city-input').val();
     const units = "imperial";
     let forecastAPIURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchedCity}&units=${units}&appid=${key}`;
@@ -56,8 +55,10 @@ const getFiveDayForecast = (event) => {
     .then((response) => {
         
         fiveDayHTML = `
-            <div id="five-day-forecast" class="col justify-content-between">
-                <h2>Five-Day Forecast:</h2>
+        
+        <h2 class="p-2">  Five-Day Forecast:</h2>
+        <div id="" class="col d-inline-flex flex-wrap">
+        
             `;
         // loop to get values for each day
         
@@ -70,17 +71,19 @@ const getFiveDayForecast = (event) => {
 
 
             fiveDayHTML += `
-                <div id="daily-forecast-card" class="row mt-5">
-                    <h3 class="five-day-date">${fiveDays} <img src="${fiveDayIcon}" alt="weather"></h3>
-                    <p>Temp: <span id="five-day-temp">${fiveDayCall.main.temp} °F</span></p>
-                    <p>Humidity: <span id="five-day-hum">${fiveDayCall.main.humidity}%</span></p>
-                    <p>Wind Speed: <span id="five-day-wind">${fiveDayCall.wind.speed} mph</span></p>
-                </div>
+                
+                    <div id="daily-forecast-card"class="card vw-10 p-3 m-2">
+                        <h3 class="five-day-date">${fiveDays} <img src="${fiveDayIcon}" alt="weather"></h3>
+                        <p>Temp: <span id="five-day-temp">${fiveDayCall.main.temp} °F</span></p>
+                        <p>Humidity: <span id="five-day-hum">${fiveDayCall.main.humidity}%</span></p>
+                        <p>Wind: <span id="five-day-wind">${fiveDayCall.wind.speed} mph</span></p>
+                    </div>
             `;
-            fiveDayHTML += `</div>`
+            
             console.log(fiveDays[i]);
             $('#five-day-forecast').html(fiveDayHTML);
         }
+        fiveDayHTML += `</div>`
     })
 
 }
